fbe731099532d15dc182e27be536476901cffe1a
Hunter Evanoff <hunter547@gmail.com>
2024-07-14 07:17:21 -0600

    Initial commit

---
+++ gatsby-node.ts
@@ -0,0 +1,13 @@
+import type { Actions } from "gatsby"
+import * as path from "path"
+
+export const onCreateWebpackConfig = ({ actions }: { actions: Actions}) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "@/components": path.resolve(__dirname, "src/components"),
+        "@/lib/utils": path.resolve(__dirname, "src/lib/utils"),
+      },
+    },
+  })
+}
